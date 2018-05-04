c7a28becc3c871cd71ededd27d1adaee84dcf94a
Sara Hamilton <sara@indeterminate.net>
2018-05-04 09:20:53 -0700

    add controllers and view models folder

---
+++ SaraHamilton/Controllers/PostsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SaraHamilton.Models;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace SaraHamilton.Controllers
+{
+    public class PostsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
