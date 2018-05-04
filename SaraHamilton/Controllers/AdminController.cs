0a57ac912891b1ded1a6a0fa88058f33406ff485
Sara Hamilton <sara@indeterminate.net>
2018-05-04 11:08:03 -0700

    add admin controller and index view for ajax functionality

--- SaraHamilton/Controllers/AdminController.cs
+++ SaraHamilton/Controllers/AdminController.cs
@@ -13,12 +13,12 @@ using SaraHamilton.Data;
 namespace SaraHamilton.Controllers
 {
     [Authorize]
-    public class PostsController : Controller
+    public class AdminController : Controller
     {
         private readonly ApplicationDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public PostsController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        public AdminController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
         {
             _userManager = userManager;
             _db = db;
@@ -28,23 +28,20 @@ namespace SaraHamilton.Controllers
         {
             var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             var currentUser = await _userManager.FindByIdAsync(userId);
-            return View(_db.Posts.Where(x => x.User.Id == currentUser.Id));
-        }
-
-        public IActionResult Create()
-        {
             return View();
         }
 
+        // AJAX method for creating a new post
         [HttpPost]
-        public async Task<IActionResult> Create(Post post)
+        public async Task<IActionResult> NewPost(string newTitle, string newContent)
         {
             var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             var currentUser = await _userManager.FindByIdAsync(userId);
-            post.User = currentUser;
-            _db.Posts.Add(post);
+            Post newPost = new Post(newTitle, newContent);
+            newPost.User = currentUser;
+            _db.Posts.Add(newPost);
             _db.SaveChanges();
-            return RedirectToAction("Index");
+            return Json(newPost);
         }
     }
 }
