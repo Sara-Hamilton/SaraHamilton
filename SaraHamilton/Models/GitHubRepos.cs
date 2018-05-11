036e53980f444e217e45180560415f97a0ccebd8
Sara Hamilton <sara@indeterminate.net>
2018-05-11 08:08:05 -0700

    add githubrepos model

---
+++ SaraHamilton/Models/GitHubRepos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace SaraHamilton.Models
+{
+    public class GitHubRepos
+    {
+    }
+}
